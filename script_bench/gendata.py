c7bd12c298518819263f6fb00f5b891d55ac4870
daly88 <dalydaijun@163.com>
2013-03-12 13:49:37 +0800

    fix lua/python call

--- script_bench/gendata.py
+++ script_bench/gendata.py
@@ -3,9 +3,9 @@ import random
 
 def gen_array():
 	arr = {}
-	for i in xrange(0, 300):
+	for i in xrange(0, 600):
 		v = random.randint(1, 100000)
-		if len(arr) > 200: break
+		if len(arr) > 500: break
 		if v not in arr:
 			arr[v] = 1
 
--- script_bench/scriptbench.cc
+++ script_bench/scriptbench.cc
@@ -11,7 +11,7 @@
 	begin_##x = timenow();
 
 #define RUN_END(x) end_##x = timenow(); \
-	printf("%s used: %.4lf s\n", #x, (double)(end_##x - begin_##x) / 10000.0);
+	printf("run:%s used: %.4lf s\n", #x, (double)(end_##x - begin_##x) / 10000.0);
 	
 
 //uint: 0.1 ms
@@ -36,18 +36,23 @@ int py_bench()
 	PyRun_SimpleString("sys.path.append('./')");
 
 
-	PyObject *pName, *pModule, *pDict;
+	PyObject *pName, *pModule, *pDict, *pFunc;
 	pName = PyString_FromString("py_main");
+
 	pModule = PyImport_Import(pName);
 	if (!pModule) {
 		printf("load pythong module fail\n");
 		return -1;	
 	}
 
+	RUN_BEGIN(py_array);
 	pDict = PyModule_GetDict(pModule);
 	if (!pDict) {
 		return -1;
 	}
+	pFunc = PyDict_GetItemString(pDict, "bench_array");
+	PyEval_CallObject(pFunc, NULL);
+	RUN_END(py_array);
 	return 0;
 }
 
@@ -60,23 +65,31 @@ int lua_bench()
 		printf("init lua failed\n");
 		return -1;
 	}
-	ret = luaL_loadfile(lstate, "lua_main.lua");
+
+	luaL_openlibs(lstate);
+	//ret = luaL_loadfile(lstate, "lua_main.lua");
+	luaL_dofile(lstate, "lua_main.lua");
 	if (ret != 0) {
 		printf("load lua file failed\n");
 		return -1;
 	}
+
+	RUN_BEGIN(lua_array);
+	lua_getglobal(lstate, "bench_array");
+	if (lua_pcall(lstate, 0, 0, 0) != 0) {
+		printf("fail to call lua function\n");
+		return -1;
+	}
+	RUN_END(lua_array);
+	lua_close(lstate);
 	return 0;
 }
 
 
 int main(int argc, char** argv)
 {
-	RUN_BEGIN(python);
 	py_bench();
-	RUN_END(python);
 
-	RUN_BEGIN(lua);
 	lua_bench();
-	RUN_END(lua);
 	return 0;
 }
