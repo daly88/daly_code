a032e49891b47b4746262393560ec31bf9f88382
daly88 <dalydaijun@163.com>
2013-05-17 09:48:52 +0800

    xldiff finish

--- xldiff/xldiff.py
+++ xldiff/xldiff.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 #coding=utf-8
-
+import sys
 import xlrd
 import hashlib
 
 INPUT_CHARSET = 'gbk'
+OUTPUT_CHARSET = 'utf-8'
+MAX_SHOWFIELD = 6
+MAX_FIELD_LEN = 8
 
 class DiffMatrix(object):
 	OLD_DEL = 1
@@ -95,81 +98,134 @@ class DiffMatrix(object):
 				p_new = p_new + 1
 
 
+class Workbook(object):
+	def __init__(self, bookname):
+		self.wb_name = bookname
+		try:
+			self.wb_ = xlrd.open_workbook(bookname)
+		except:
+			print 'unable to open workbook %s' % bookname
+			sys.exit(-1)
 
-class SheetMgr(object):
-	def __init__(self, workbook, name):
+	def sheet_by_index(self, index):
+		return self.wb_.sheet_by_index(index)
+
+	def nsheets(self):
+		return self.wb_.nsheets
+	
+	def find_index(name):
+		names = self.wb_.sheet_names()
+		i = 1
+		for s in names:
+			if name == s: return i
+			i = i + 1
+		return -1
+
+class SingleSheet(object):
+	def __init__(self, workbook, index):
 		self.row_digest = []
 		self.title_ = []
-		self.wb_name = workbook
-		self.wb_ = xlrd.open_workbook(workbook)
-		self.sheet_name = name
+		self.wb_ = workbook
 
-		self.sheet_ = self.wb_.sheet_by_name(name)
+		self.sheet_ = self.wb_.sheet_by_index(index)
 		self.ncols_ = self.sheet_.ncols
 		self.nrows_ = self.sheet_.nrows
 		for j in xrange(self.ncols_):
-			self.sheet_.cell(0, j).value
+			self.title_.append(self.sheet_.cell(0, j).value)
 
 	
 	def serialize_row(self, row):
 		field = []
 		for col in xrange(self.ncols_):	
-			try:
-				node = self.sheet_.cell(row-1, col)
-			except:
-				return '??error?? %d %d' % (row, col)
-			if node.ctype == xlrd.XL_CELL_NUMBER:
-				field.append(unicode(str(int(node.value))))
-			elif node.ctype == xlrd.XL_CELL_TEXT:
-				field.append(node.value)
+			field.append(self.str_value(row-1, col))
 
-		msg = ''
+		msg = u''
 		i = 0
 		for f in field:
-			if len(f) > 20: f = f[:20] + '....'
+			if i > MAX_SHOWFIELD: 
+				msg = msg + u'......' 
+				break
+			if len(f) > MAX_FIELD_LEN: f = f[:MAX_FIELD_LEN] + u'....'
 			try:
-				msg = msg + ' ' + f.encode('utf-8')
+				msg = msg + u' ' + f
 				i = i + 1
 			except:
-				msg = msg + '????'
+				msg = msg + u'????'
+
 		return msg
+
+	def str_value(self, i, j):
+		node = self.sheet_.cell(i, j)
+		if node.ctype == xlrd.XL_CELL_NUMBER: 
+			return unicode(int(node.value))
+		elif node.ctype == xlrd.XL_CELL_TEXT: 
+			return node.value
+		else:
+			return u''
+		
 
 	def read_row_digest(self):
 		for i in xrange(0, self.nrows_):
-			msg = ''
+			msg = u''
 			for j in xrange(self.ncols_):
-				node = self.sheet_.cell(i, j)
-				if node.ctype == xlrd.XL_CELL_NUMBER:
-					msg = msg + str(node.value)
-				elif node.ctype == xlrd.XL_CELL_TEXT:
-					msg = msg + node.value
+				msg = msg + self.str_value(i, j)
+				
 			m = hashlib.md5(msg.encode(INPUT_CHARSET))
 			self.row_digest.append(m.hexdigest())
 
 	def dump_digest(self):
 		for v in self.row_digest: print v
 
-
 def output_diff(sh_old, sh_new):
+	sh_old.read_row_digest()
+	sh_new.read_row_digest()
 	df = DiffMatrix(sh_old.row_digest, sh_new.row_digest)
-	#df = DiffMatrix(a,b)
 	df.fill_lcs_matrix()
 	df.traceback_matrix()
-	for i in df.pure_del: 
-		print '- ' + sh_old.serialize_row(i)
-	for i in df.pure_add: 
-		print '+ ' + sh_new.serialize_row(i)
-	for i in df.modify: 
-		print '!- ' + sh_old.serialize_row(i[0])
-		print '!+ ' + sh_new.serialize_row(i[1])
+	print u'@@ %s @@' % sh_new.sheet_.name
+	if len(df.pure_del) > 0:
+		print '===DEL==='
+		for i in df.pure_del: print u'- %d ' % i + sh_old.serialize_row(i)
+	if len(df.pure_add) > 0:
+		print '===ADD==='
+		for i in df.pure_add: print u'+ %d ' % i + sh_new.serialize_row(i)
+	if len(df.modify) > 0:
+		print '===MOD==='
+		for i in df.modify: 
+			msg1 = u''
+			msg2 = u''
+			for f in xrange(sh_old.ncols_):
+				v1 = sh_old.str_value(i[0]-1, f)
+				v2 = sh_new.str_value(i[1]-1, f)
+				if len(v1) > MAX_FIELD_LEN: v1 = v1[:MAX_FIELD_LEN] + u'....'
+				if len(v2) > MAX_FIELD_LEN: v2 = v2[:MAX_FIELD_LEN] + u'....' 
+				if v1 != v2:
+					msg1 = msg1 + u'%s: %s   ' % (sh_old.title_[f], v1)
+					msg2 = msg2 + u'%s: %s   ' % (sh_new.title_[f], v2)
+
+			print u'- %d ' % i[0] + sh_old.str_value(i[0]-1, 0) + u' ' + msg1
+			print u'+ %d ' % i[1] + sh_new.str_value(i[1]-1, 0) + u' ' + msg2
 
 
+def do_diff(book_old, book_new):
+	wb_old = Workbook(book_old)
+	wb_new = Workbook(book_new)
+	print 'Index: %s' % book_old
+	print '======================================'
+	if wb_old.nsheets() != wb_new.nsheets():
+		print 'diffrent sheet count'
+		return 
+	scount = wb_old.nsheets()
+	for i in xrange(scount):
+		sa = SingleSheet(wb_old, i)
+		sb = SingleSheet(wb_new, i)
+		if sa.sheet_.name != sb.sheet_.name: continue
+		output_diff(sa, sb)
+	
 if __name__ == '__main__':
-	sh1 = SheetMgr('books.xls', 'legend')
-	sh1.read_row_digest()
-	#sh1.dump_digest()
-	sh2 = SheetMgr('books2.xls', 'legend')
-	sh2.read_row_digest()
-	#sh2.dump_digest()
-	output_diff(sh1, sh2)
+	args = sys.argv
+	if len(args) < 2:
+		print 'xldiff book_old book_new' 
+		sys.exit(0)
+	do_diff(args[1], args[2])
 
