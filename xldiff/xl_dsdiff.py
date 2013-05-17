ac7c3cc651028e5986f034d2b9720cee1aacb32e
daly88 <dalydaijun@163.com>
2013-05-17 13:14:58 +0800

    add xl_dsdiff for game data config

--- xldiff/xl_dsdiff.py
+++ xldiff/xl_dsdiff.py
@@ -1,109 +1,52 @@
 #!/usr/local/bin/python
 #
-# general Excel diff for data config datasheet 
+#
+# Excel diff for specific game data config datasheet 
 # the format of datasheet:
+#     column 0: the unique id to identify unique row config
+#               suppose there's no conflict id in column 0
 #     row 0: the column title
+#
 # two sheet must have the same amount of sheet with the same sheet name
 # Author: Daly 2013-5
-
 
 import sys
 import xlrd
 import hashlib
 
 INPUT_CHARSET = 'gbk'
-OUTPUT_CHARSET = 'utf-8'
 MAX_SHOWFIELD = 6
 MAX_FIELD_LEN = 8
 
-class DiffTool(object):
-	OLD_DEL = 1
-	NEW_ADD = 2
-	def __init__(self, m_tokens, n_tokens):
-		mat = []
-		m = len(m_tokens)
-		n = len(n_tokens)
-		self.m_tokens = m_tokens
-		self.n_tokens = n_tokens
-		for i in xrange(0, m+1):
-			mat.append([])
-			for j in xrange(0, n+1):
-				mat[i].append(0)
-
-		self.mat = mat
-		self.m = m
-		self.n = n
-
-	def fill_lcs_matrix(self):
-		p,q,r = 0,0,0
-		for i in xrange(1, self.m+1):
-			for j in xrange(1, self.n+1):
-				p = self.mat[i-1][j-1]
-				if self.m_tokens[i-1] == self.n_tokens[j-1]:  p = p + 1
-				q = self.mat[i-1][j]
-				r = self.mat[i][j-1]
-				self.mat[i][j] = max(p,q,r)
-
-	def traceback_matrix(self):
-		p,q,v = 0,0,0
-		stack = []
-		i = self.m
-		j = self.n	
-		#trace back LCS matrix
-		while i > 0 or j > 0:
-			if i == 0:
-				stack.append((self.NEW_ADD, j))
-				j = j - 1
-				continue
-			if j == 0:
-				stack.append((self.OLD_DEL, i))
-				i = i - 1
-				continue
-
-			p = self.mat[i-1][j]
-			q = self.mat[i][j-1]
-			v = self.mat[i][j]
-			if p == q:
-				if v == p: stack.append((self.OLD_DEL,i))
-				else: j = j - 1
-				i = i - 1
-			elif p > q:
-				stack.append((self.OLD_DEL,i))  #go vertical
-				i = i - 1
-			else:
-				stack.append((self.NEW_ADD,j))  #go horizontal
-				j = j - 1
-		stack.reverse()
-		self.result_stack = stack
-		#make raw diff(+/-)
-		self.old_del = {}
-		self.new_add = {}
-		for r in stack:
-			if r[0] == self.NEW_ADD:  self.new_add[r[1]] = 1
-			else:  self.old_del[r[1]] = 1
-
-		#analyze pretty diff info
-		#identify modify/pure_add/pure_del
-		p_old, p_new = 1, 1
+class DsDiffTool(object):
+	def __init__(self, sh_old, sh_new):
+		self.sh_old = sh_old
+		self.sh_new = sh_new
 		self.pure_del = []
 		self.pure_add = []
 		self.modify = []
+	
+	def compute_diff(self):
+		old_row = None
+		for id, row in self.sh_old.unique_id_row.items():
+			if id not in self.sh_new.unique_id_row:
+				self.pure_del.append(row)
 
-		while p_old <= self.m or p_new <= self.n:
-			if p_old in self.old_del:
-				if p_new in self.new_add:
-					self.modify.append((p_old, p_new))
-					p_new = p_new + 1
-				else:
-					self.pure_del.append(p_old)
-				p_old = p_old + 1
-			elif p_new in self.new_add:
-				self.pure_add.append(p_new)
-				p_new = p_new + 1
+		for id, row in self.sh_new.unique_id_row.items():
+			if id not in self.sh_old.unique_id_row:
+				self.pure_add.append(row)
 			else:
-				p_old = p_old + 1
-				p_new = p_new + 1
-
+				old_row = self.sh_old.unique_id_row[id]
+				ret = self.compare_row(old_row, row)
+				if ret != -1:
+					self.modify.append((old_row, row))
+	
+	def compare_row(self, r1, r2):
+		for col in xrange(self.sh_new.ncols_):
+			v1 = self.sh_old.sheet_.cell(r1, col).value
+			v2 = self.sh_new.sheet_.cell(r2, col).value
+			if v1 != v2: return col
+		return -1
 
 
 class Workbook(object):
@@ -131,7 +74,7 @@ class Workbook(object):
 
 class DataSheet(object):
 	def __init__(self, workbook, index):
-		self.row_digest = []
+		self.unique_id_row = {}
 		self.title_ = []
 		self.wb_ = workbook
 
@@ -140,6 +83,11 @@ class DataSheet(object):
 		self.nrows_ = self.sheet_.nrows
 		for j in xrange(self.ncols_):
 			self.title_.append(self.sheet_.cell(0, j).value)
+
+		pid = 0
+		for i in xrange(self.nrows_):
+			pid = self.str_value(i, 0)
+			self.unique_id_row[pid] = i
 
 	
 	def serialize_row(self, row):
@@ -172,25 +120,9 @@ class DataSheet(object):
 			return u''
 		
 
-	def read_row_digest(self):
-		for i in xrange(0, self.nrows_):
-			msg = u''
-			for j in xrange(self.ncols_):
-				msg = msg + self.str_value(i, j)
-				
-			m = hashlib.md5(msg.encode(INPUT_CHARSET))
-			self.row_digest.append(m.hexdigest())
-
-	def dump_digest(self):
-		for v in self.row_digest: print v
-
 def output_diff(sh_old, sh_new):
-	sh_old.read_row_digest()
-	sh_new.read_row_digest()
-
-	df = DiffTool(sh_old.row_digest, sh_new.row_digest)
-	df.fill_lcs_matrix()
-	df.traceback_matrix()
+	df = DsDiffTool(sh_old, sh_new)
+	df.compute_diff()
 	print u'@@ %s @@' % sh_new.sheet_.name
 	if len(df.pure_del) > 0:
 		print '===DEL==='
@@ -204,11 +136,11 @@ def output_diff(sh_old, sh_new):
 			msg1 = u''
 			msg2 = u''
 			for f in xrange(sh_old.ncols_):
-				v1 = sh_old.str_value(i[0]-1, f)
-				v2 = sh_new.str_value(i[1]-1, f)
-				if len(v1) > MAX_FIELD_LEN: v1 = v1[:MAX_FIELD_LEN] + u'....'
-				if len(v2) > MAX_FIELD_LEN: v2 = v2[:MAX_FIELD_LEN] + u'....' 
+				v1 = sh_old.str_value(i[0], f)
+				v2 = sh_new.str_value(i[1], f)
 				if v1 != v2:
+					if len(v1) > MAX_FIELD_LEN: v1 = v1[:MAX_FIELD_LEN] + u'....'
+					if len(v2) > MAX_FIELD_LEN: v2 = v2[:MAX_FIELD_LEN] + u'....' 
 					msg1 = msg1 + u'%s: %s   ' % (sh_old.title_[f], v1)
 					msg2 = msg2 + u'%s: %s   ' % (sh_new.title_[f], v2)
 
@@ -216,11 +148,9 @@ def output_diff(sh_old, sh_new):
 			print u'+ %d ' % i[1] + sh_new.str_value(i[1]-1, 0) + u' ' + msg2
 
 
-def do_diff(book_old, book_new):
-	wb_old = Workbook(book_old)
+def do_diff(book_old, book_new):	
 	wb_new = Workbook(book_new)
-	#print 'Index: %s' % book_new
-	#print '======================================'
+	wb_old = Workbook(book_old)
 	if wb_old.nsheets() != wb_new.nsheets():
 		print 'diffrent sheet count'
 		return 
